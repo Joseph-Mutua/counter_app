24ba770d5a65193e895b233118e4bd462007fc28
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-17 23:24:38 +0300

    Embed an expression

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class Counter extends Component { 
-    render() { 
-        return (
-          <React.Fragment>
-            <h1>Hello World</h1>
-            <button>Increment</button>
-          </React.Fragment>
-        );
-    }
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
