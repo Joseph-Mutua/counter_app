50808bb41c43d09e3460336cde8cb3a6d889400f
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-18 12:48:46 +0300

    Update the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -37,8 +37,8 @@ class Counter extends Component {
   // };
 
   handleIncrement = () => {
-    console.log("Increment Clicked!", this);
-  }
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     let classes = this.getBadgeClasses();
