db3e5606fc0d217f10ed0c2218e2202dccfd0b93
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-23 17:04:55 +0300

    Lifting the state up

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
+  //   this.handleDelete = this.handleDelete.bind(this);
   // }
 
   // state = {
@@ -49,13 +50,17 @@ class Counter extends Component {
           {/* <h4>{this.props.id}</h4> */}
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
           <button
-            onClick={() => this.props.onIncrement(this.props.counter)}
+            onClick={() => {
+              this.props.onIncrement(this.props.counter);
+            }}
             className="btn btn-secondary btn-sm"
           >
             Increment
           </button>
           <button
-            onClick={() => this.props.onDelete(this.props.counter.id)}
+            onClick={() => {
+              this.props.onDelete(this.props.counter.id);
+            }}
             className="btn btn-danger btn-sm m-2"
           >
             Delete
@@ -77,7 +82,7 @@ class Counter extends Component {
   }
 
   formatCount() {
-    const { value }  = this.props.counter;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
